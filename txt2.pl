b3f8dcae4ce3264cd993b62f8df7142b80294ebb
MeghaDesai511 <33537147+MeghaDesai511@users.noreply.github.com>
2017-11-17 09:43:02 +0530

    Update txt2.pl

--- txt2.pl
+++ txt2.pl
@@ -1,12 +1,67 @@
 use Tk;
 use Tk::BrowseEntry;
 use strict;
+use warnings;
 
 my $mw = MainWindow->new(-title => 'Text Editor');
 my $f = $mw->Frame->pack(-side => 'top');
 
 #my $textarea = $mw -> Frame(); #Creating Another Frame
 #my $txt = $textarea -> Text(-width=>40, -height=>10);
+
+my @fgColors = (
+  'default', 'bold', 'black', 'red', 'blue', 'yellow', 'green',
+  'majenta', 'cyan', 'white', 'bold black', 'bold red', 'bold blue',
+  'bold yellow', 'bold green', 'bold majenta', 'bold cyan', 'bold whit
++e');
+
+my @bgColors = (
+  'default', 'black', 'red', 'blue', 'yellow', 'green', 'majenta', 'cy
++an', 'white');
+
+my %fg = (
+  'default'      => "",
+  'bold'         => "\e[1m",
+  'black'        => "\e[30m",
+  'red'          => "\e[31m",
+  'blue'         => "\e[32m",
+  'yellow'       => "\e[33m",
+  'green'        => "\e[34m",
+  'majenta'      => "\e[35m",
+  'cyan'         => "\e[36m",
+  'white'        => "\e[37m",
+  'bold black'   => "\e[1;30m",
+  'bold red'     => "\e[1;31m",
+  'bold blue'    => "\e[1;32m",
+  'bold yellow'  => "\e[1;33m",
+  'bold green'   => "\e[1;34m",
+  'bold majenta' => "\e[1;35m",
+  'bold cyan'    => "\e[1;36m",
+  'bold white'   => "\e[1;37m",
+);
+
+my %bg = (
+  'default'      => "",
+  'black'        => "\e[40m",
+  'red'          => "\e[41m",
+  'blue'         => "\e[42m",
+  'yellow'       => "\e[43m",
+  'green'        => "\e[44m",
+  'majenta'      => "\e[45m",
+  'cyan'         => "\e[46m",
+  'white'        => "\e[47m");
+
+print "                e[40m e[41m e[42m e[43m e[44m e[45m e[46m e[47m
++\n";
+foreach my $fgc (@fgColors)
+{
+  my $printable = $fg{$fgc};
+  $printable =~ s/\e/e/;
+  printf "%9s ", $printable;
+
+  print "$fg{$fgc}$bg{$_} Text \e[0m" for @bgColors;
+  print "\n";
+}
 
 $mw->geometry('1000x800');
 
